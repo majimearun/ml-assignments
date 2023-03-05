113ad826b90c2e10a80d8833b5e44ae211eb98f9
majimearun <arunachala0619@gmail.com>
2023-03-05 12:58:48 +0530

    shifted template to all 3 folders

---
+++ Logisitc Regression/template.py
@@ -0,0 +1,16 @@
+# import numpy as np
+
+# class Model:
+#     def __init__():
+        
+        
+#     def fit(self, X: np.ndarray, y: np.ndarray):
+
+                
+#     def predict(self, X: np.ndarray) -> np.ndarray:
+        
+    
+#     def _classify(self, x: np.ndarray) -> np.ndarray:
+        
+    
+#     def score(self, X: np.ndarray, y: np.ndarray) -> float:
