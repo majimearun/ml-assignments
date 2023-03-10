9348c86d7a5e5dfa59ebd0276045ae532481706b
majimearun <arunachala0619@gmail.com>
2023-03-10 12:31:22 +0530

    set default list top -1, 1

--- Fischers LDA/FischersLinearDiscriminant.py
+++ Fischers LDA/FischersLinearDiscriminant.py
@@ -2,32 +2,33 @@ import numpy as np
 
 
 class GaussianDistribution:
-    
     def __init__(self, mean: float, std: float):
         """Gaussian distribution class
-        
+
         Args:
             mean (float): Mean of the distribution
             std (float): Standard deviation of the distribution
         """
         self.mean = mean
         self.std = std
-        
+
     def find_probability(self, x):
         """Find the probability of a point being in a distribution
-        
+
         Args:
             x (float): Point to find the probability of
-            
+
         Returns:
             float: Probability of the point being in the distribution
         """
-        return np.exp(-((x - self.mean) ** 2) / (2 * (self.std ** 2)))/(np.sqrt (2 * np.pi) * self.std)
+        return np.exp(-((x - self.mean) ** 2) / (2 * (self.std**2))) / (
+            np.sqrt(2 * np.pi) * self.std
+        )
+
 
 class FischersLinearDiscriminant:
     def __init__(self):
-        """Fischers Linear Discriminant Classifier
-        """
+        """Fischers Linear Discriminant Classifier"""
         self._w: np.ndarray = None
         self._class1_prior: float = 0
         self._class2_prior: float = 0
@@ -36,93 +37,117 @@ class FischersLinearDiscriminant:
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         """Finds the line onto which the data can be projected to maximise the separation between the two classes and minimises the variance within each class. Also finds and sets the priors and gaussian distributions for each class.
-        
+
         Args:
             X (np.ndarray): Data to fit the model to
             y (np.ndarray): Labels for the data
         """
         X_class1 = [X[i] for i in range(len(X)) if y[i] == 1]
         X_class2 = [X[i] for i in range(len(X)) if y[i] == -1]
-
         class1_mean = np.mean(X_class1, axis=0)
         class2_mean = np.mean(X_class2, axis=0)
-        
-        s1_squared = np.mean([(x - class1_mean).reshape(-1, 1) @ (x - class1_mean).reshape(1, -1) for x in X_class1], axis=0)
-        s2_squared = np.mean([(x - class1_mean).reshape(-1, 1) @ (x - class1_mean).reshape(1, -1) for x in X_class2], axis=0)
-        
+
+        s1_squared = np.sum(
+            [
+                (x - class1_mean).reshape(-1, 1) @ (x - class1_mean).reshape(1, -1)
+                for x in X_class1
+            ],
+            axis=0,
+        )
+        s2_squared = np.sum(
+            [
+                (x - class1_mean).reshape(-1, 1) @ (x - class1_mean).reshape(1, -1)
+                for x in X_class2
+            ],
+            axis=0,
+        )
+
         sw = s1_squared + s2_squared
         self._w = np.linalg.inv(sw) @ (class1_mean - class2_mean).reshape(-1, 1)
 
-        
         projected_class1 = np.array([self._project(x) for x in X_class1])
         projected_class2 = np.array([self._project(x) for x in X_class2])
-        
-        self._class1_gaussian = GaussianDistribution(np.mean(projected_class1), np.std(projected_class1))
-        self._class2_gaussian = GaussianDistribution(np.mean(projected_class2), np.std(projected_class2))
-        
-        self._class1_prior = len(X_class1)/len(X)
-        self._class2_prior = len(X_class2)/len(X)
-        
-        
-    
+
+        self._class1_gaussian = GaussianDistribution(
+            np.mean(projected_class1), np.std(projected_class1)
+        )
+        self._class2_gaussian = GaussianDistribution(
+            np.mean(projected_class2), np.std(projected_class2)
+        )
+
+        self._class1_prior = len(X_class1) / len(X)
+        self._class2_prior = len(X_class2) / len(X)
+
     def _project(self, x) -> float:
         """Projects a point onto the line (discriminant) found by the fit method
-        
+
         Args:
             x (np.ndarray): Point to project onto the line
-            
+
         Returns:
             float: Projection of the point onto the line
         """
         return x @ self._w
-        
-        
+
     def predict(self, X: np.ndarray) -> np.ndarray:
         """Predicts the classes of a set of points
-        
+
         Args:
             X (np.ndarray): Points to predict the classes of
-            
+
         Returns:
             np.ndarray: Predicted classes of the points (1D array)
         """
         return np.array([self._classify(x) for x in X])
-        
-    
+
     def _classify(self, x: np.ndarray) -> int:
         """Classifies a point as either class 1 or class -1
-        
+
         Args:
             x (np.ndarray): Point to classify
-            
+
         Returns:
             int: Class of the point
         """
-        decision = np.log(self._class1_prior) - np.log(self._class2_prior) + np.log(self._class1_gaussian.find_probability(x @ self._w)) - np.log(self._class2_gaussian.find_probability(x @ self._w))
+        decision = (
+            np.log(self._class1_prior)
+            - np.log(self._class2_prior)
+            + np.log(self._class1_gaussian.find_probability(x @ self._w))
+            - np.log(self._class2_gaussian.find_probability(x @ self._w))
+        )
         if decision > 0:
             return 1
         else:
             return -1
-        
-    
-    def score(self, X: np.ndarray, y: np.ndarray, _print: bool = False) -> tuple[float, float, float, float]:
+
+    def score(
+        self, X: np.ndarray, y: np.ndarray, _print: bool = False
+    ) -> tuple[float, float, float, float]:
         """Scores the model by calculating and printing the accuracy, precisions and recalls on the given data
-        
+
         Args:
             X (np.ndarray): Data to score the model on
             y (np.ndarray): Labels for the data
             _print (bool, optional): Whether to print the scores. Defaults to False.
-            
+
         Returns:
             tuple[float, float, float, float]: True positives, true negatives, false positives and false negatives
         """
-        
+
         predicted = self.predict(X)
-        true_positives = np.sum([1 for i in range(len(predicted)) if predicted[i] == 1 and y[i] == 1])
-        true_negatives = np.sum([1 for i in range(len(predicted)) if predicted[i] == -1 and y[i] == -1])
-        false_positives = np.sum([1 for i in range(len(predicted)) if predicted[i] == 1 and y[i] == -1])
-        false_negatives = np.sum([1 for i in range(len(predicted)) if predicted[i] == -1 and y[i] == 1])
-        
+        true_positives = np.sum(
+            [1 for i in range(len(predicted)) if predicted[i] == 1 and y[i] == 1]
+        )
+        true_negatives = np.sum(
+            [1 for i in range(len(predicted)) if predicted[i] == -1 and y[i] == -1]
+        )
+        false_positives = np.sum(
+            [1 for i in range(len(predicted)) if predicted[i] == 1 and y[i] == -1]
+        )
+        false_negatives = np.sum(
+            [1 for i in range(len(predicted)) if predicted[i] == -1 and y[i] == 1]
+        )
+
         if _print:
             print("--------Results--------")
             print(f"Accuracy: {(true_positives + true_negatives)/len(predicted)}")
@@ -132,7 +157,5 @@ class FischersLinearDiscriminant:
             print("----Class -1----")
             print(f"Precision: {true_negatives/(true_negatives + false_negatives)}")
             print(f"Recall: {true_negatives/(true_negatives + false_positives)}")
-            
+
         return true_positives, true_negatives, false_positives, false_negatives
-        
-        
--- preprocessor.py
+++ preprocessor.py
@@ -1,55 +1,55 @@
 import pandas as pd
 import numpy as np
 
+
 class Preprocessor:
     def __init__(self, data: pd.DataFrame, label_column: str):
         """Preprocesses data for use
-        
+
         Args:
             data (pd.DataFrame): Data to preprocess
             label_column (str): Name of the column containing the labels
         """
         self.original_data = data.copy()
         self.data = data.copy()
-        self.label_column = label_column    
-    
+        self.label_column = label_column
+
     def _drop_rows(self, rows: list[int]):
         """Drops rows from the data
-        
+
         Args:
             rows (list[int]): List of row indexes to drop
-            
+
         Returns:
             None
-            
+
         """
         self.data.drop(rows, inplace=True)
 
     def _standardize(self, col: pd.Series) -> pd.Series:
         """Standardizes a column
-        
+
         Args:
             col (pd.Series): Column to standardize
-            
+
         Returns:
             pd.Series: Standardized column
         """
         return (col - col.mean()) / col.std()
-    
+
     def _standardize_cols(self):
-        """Standardizes all columns except the label column
-        """
+        """Standardizes all columns except the label column"""
         for col in self.data.columns:
             if col != self.label_column:
                 self.data[col] = self._standardize(self.data[col])
-            
+
     def _impute(self, col: pd.Series, method: str = "mean") -> pd.Series:
         """Imputes missing values in a column
-        
+
         Args:
             col (pd.Series): Column to impute
             method (str, optional): Method to use for imputation. Defaults to "mean".
-            
+
         Returns:
             pd.Series: Imputed column
         """
@@ -61,48 +61,51 @@ class Preprocessor:
             return col.fillna(col.mode()[0])
         else:
             return col
-        
+
     def _impute_cols(self, impute_method: str):
-        """Imputes missing values in all columns except the label column
-        """
+        """Imputes missing values in all columns except the label column"""
         for col in self.data.columns:
             if col != self.label_column:
                 self.data[col] = self._impute(self.data[col], impute_method)
-        
+
     def _label_encode(self, labels: list[int] = [-1, 1]) -> pd.Series:
         """Encodes the label column (two classes only: +1 and -1)
-        
+
         Args:
             labels (list[int], optional): A list of what you want the labels to be. Defaults to [0, 1]
-        
+
         Returns:
             pd.Series: Encoded label column
         """
         col: pd.Series = self.data[self.label_column]
         encoded = col.astype("category").cat.codes
-        transformation_dict = {
-            0: labels[0],
-            1: labels[1]
-        }
+        transformation_dict = {0: labels[0], 1: labels[1]}
         return encoded.map(transformation_dict)
-    
+
     def _get_rows_with_missing_values(self) -> list[int]:
         """Gets the indexes of rows with missing values
-        
+
         Returns:
             list[int]: List of row  which have missing values
         """
         return self.data[self.data.isna().any(axis=1)].index.tolist()
-    
-    def preprocess(self, impute_method: str = "mean", drop_rows: list = [], drop_na: bool = True, standardize: bool = True, labels: list[int] = [0, 1]) -> pd.DataFrame:
+
+    def preprocess(
+        self,
+        impute_method: str = "mean",
+        drop_rows: list = [],
+        drop_na: bool = True,
+        standardize: bool = True,
+        labels: list[int] = [-1, 1],
+    ) -> pd.DataFrame:
         """Preprocesses the data
-        
+
         Args:
             impute_method (str, optional): Method to use for imputation. Defaults to "mean".
             drop_rows (list, optional): List of row indexes to drop. Defaults to [].
             drop_na (bool, optional): Whether to drop rows with missing values. Defaults to True.
             standardize (bool, optional): Whether to standardize the data. Defaults to True.
-        
+
         Returns:
             pd.DataFrame: Preprocessed data
         """
@@ -115,13 +118,13 @@ class Preprocessor:
             self._standardize_cols()
         self.data[self.label_column] = self._label_encode(labels)
         return self.data.copy()
-    
+
     def get_folds(self, k: int = 10) -> list[pd.DataFrame]:
         """Returns k folds of the data after shuffling
-        
+
         Args:
             k (int, optional): Number of folds. Defaults to 10.
-            
+
         Returns:
             list[pd.DataFrame]: List of folds
         """
@@ -129,4 +132,3 @@ class Preprocessor:
         data = data.sample(frac=1).reset_index(drop=True)
         folds = np.array_split(data, k)
         return folds
-    
