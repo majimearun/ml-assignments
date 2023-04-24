d93b7ca9cdf3fb138bcc315df089e03458bc3842
majimearun <arunachala0619@gmail.com>
2023-04-24 11:52:59 +0530

    feat: add laplace smoothening

--- Assignment 2/NaiveBayes.py
+++ Assignment 2/NaiveBayes.py
@@ -14,7 +14,7 @@ class Distribution:
 
     def get_conditional(self, attribute: str, value: typing.Any):
         if self.conditionals.get((attribute, value)) is None:
-            return 1e-10
+            return None
         return self.conditionals[(attribute, value)]
 
 
@@ -28,7 +28,7 @@ class NaiveBayes:
         self.labels = list(data[target].unique())
         self._train()
 
-    def _train(self):
+    def _train(self, alpha: float = 1e-10):
         # Calculate priors
         for label in self.labels:
             prior = len(self.data[self.data[self.target] == label]) / len(self.data)
@@ -38,12 +38,18 @@ class NaiveBayes:
         for label in self.labels:
             for attribute in self.attributes:
                 for value in self.data[attribute].unique():
-                    probability = len(
-                        self.data[
-                            (self.data[attribute] == value)
-                            & (self.data[self.target] == label)
-                        ]
-                    ) / len(self.data[self.data[self.target] == label])
+                    probability = (
+                        len(
+                            self.data[
+                                (self.data[attribute] == value)
+                                & (self.data[self.target] == label)
+                            ]
+                        )
+                        + alpha
+                    ) / (
+                        len(self.data[self.data[self.target] == label])
+                        + alpha * len(self.attributes)
+                    )
                     self.distributions[label].add_conditional(
                         attribute, value, probability
                     )
@@ -56,9 +62,20 @@ class NaiveBayes:
             for label in self.labels:
                 posterior = self.distributions[label].prior
                 for attribute in self.attributes:
-                    posterior *= self.distributions[label].get_conditional(
-                        attribute, row[attribute]
-                    )
+                    if (
+                        self.distributions[label].get_conditional(
+                            attribute, row[attribute]
+                        )
+                        is None
+                    ):
+                        posterior *= 1 / (
+                            len(self.data[self.data[self.target] == label])
+                            + len(self.attributes)
+                        )
+                    else:
+                        posterior *= self.distributions[label].get_conditional(
+                            attribute, row[attribute]
+                        )
                 if posterior > max_posterior:
                     max_posterior = posterior
                     max_label = label
